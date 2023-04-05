3816a59cc708e861f062b8c3adb5dc7ebffa4133
Willian Gonçalves <willian.p.g@outlook.com>
2023-04-05 10:19:35 -0300

    Iniciando o exercicio seção 1

--- src/functions.js
+++ src/functions.js
@@ -1,12 +1,15 @@
 // 1 - Crie a função ligarDesligar, que ligue e desligue um motor de um carro.
-
-
-// 2 - Crie a função circleArea, que calcule a área de um círculo. 
-
-
+let ligarDesligar = (carro) => {
+  if (carro === 'desligado') {
+    carro = 'ligado';
+  } else {
+    carro = 'desligado';
+  }
+  console.log(`O motor está ${carro}`);
+};
+ligarDesligar('ligado');
+// 2 - Crie a função circleArea, que calcule a área de um círculo.
 // 3 - Crie a função longestWord, que receba uma frase como parâmetro e retorne a maior palavra da frase.
-
-
 // Não modifique as linhas abaixo
 module.exports = {
   ligarDesligar: typeof ligarDesligar === 'function' ? ligarDesligar : (() => {}),
