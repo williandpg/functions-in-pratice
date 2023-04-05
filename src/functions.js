c7f62d86342c1a8b364439ed5efa6acaa2eb7183
Willian Gonçalves <willian.p.g@outlook.com>
2023-04-05 10:59:56 -0300

    Ajustes das primeiras seções

--- src/functions.js
+++ src/functions.js
@@ -5,17 +5,19 @@ let ligarDesligar = (carro) => {
   } else {
     carro = 'desligado';
   }
+  console.log(`O motor está ${carro}`);
 };
 // 2 - Crie a função circleArea, que calcule a área de um círculo.
 const circleArea = (raio) => {
   const pi = 3.14;
   if (typeof raio !== 'number') {
-    return 'O parâmetro deve ser um número';
+    return 'O parâmetro raio deve ser um número';
   }
-  let area = pi * (raio ** 2);
-  return `Esta é a área do circulo: ${area}`
+  let area = pi * (raio * raio);
+  return `Esta é a área do circulo: ${area}`;
 };
 // 3 - Crie a função longestWord, que receba uma frase como parâmetro e retorne a maior palavra da frase.
+
 // Não modifique as linhas abaixo
 module.exports = {
   ligarDesligar: typeof ligarDesligar === 'function' ? ligarDesligar : (() => {}),
