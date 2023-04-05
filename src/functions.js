44aa418ebfbee93a82d8bca24928e261798c1f9b
Willian Gonçalves <willian.p.g@outlook.com>
2023-04-05 10:34:39 -0300

    Iniciando a seção 2

--- src/functions.js
+++ src/functions.js
@@ -5,10 +5,16 @@ let ligarDesligar = (carro) => {
   } else {
     carro = 'desligado';
   }
-  console.log(`O motor está ${carro}`);
 };
-ligarDesligar('ligado');
 // 2 - Crie a função circleArea, que calcule a área de um círculo.
+const circleArea = (raio) => {
+  const pi = 3.14;
+  if (typeof raio !== 'number') {
+    return 'O parâmetro deve ser um número';
+  }
+  let area = pi * (raio ** 2);
+  return `Esta é a área do circulo: ${area}`
+};
 // 3 - Crie a função longestWord, que receba uma frase como parâmetro e retorne a maior palavra da frase.
 // Não modifique as linhas abaixo
 module.exports = {
