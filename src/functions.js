ac54bafdc09c2c6036d87a71a83df62f816a5d4b
Willian Gonçalves <willian.p.g@outlook.com>
2023-04-05 11:33:30 -0300

    Update das seções

--- src/functions.js
+++ src/functions.js
@@ -11,14 +11,26 @@ let ligarDesligar = (carro) => {
 // 2 - Crie a função circleArea, que calcule a área de um círculo.
 const circleArea = (raio) => {
   const pi = 3.14;
-  if (typeof raio !== 'number') {
-    return 'O parâmetro radius deve ser um número';
+  if (typeof (raio) === 'number') {
+    const area = pi * (raio ** 2);
+    return `Essa é a área do círculo: ${area}`;
   }
-  let area = pi * (raio ** 2);
-  return `Esta é a área do círculo: ${area}`;
+  return 'O parâmetro radius deve ser um número';
 };
 // 3 - Crie a função longestWord, que receba uma frase como parâmetro e retorne a maior palavra da frase.
+const longestWord = (frase) => {
+  const array = frase.split(' ');
+  let tamanhoPalavra = 0;
+  let maiorPalavra = '';
 
+  for (const palavra of array) {
+    if (palavra.length > tamanhoPalavra) {
+      tamanhoPalavra = palavra.length;
+      maiorPalavra = palavra;
+    }
+  }
+  return maiorPalavra;
+}
 // Não modifique as linhas abaixo
 module.exports = {
   ligarDesligar: typeof ligarDesligar === 'function' ? ligarDesligar : (() => {}),
